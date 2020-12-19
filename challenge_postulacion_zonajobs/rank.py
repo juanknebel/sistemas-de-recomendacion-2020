c6f08c60064f5510b3edfcb9ef988ce047dc5ed4
Juan Knebel <juanknebel@gmail.com>
2020-12-18 23:40:44 -0300

    21_intento_35_componentes_150_epocas_todo_lightfm_uf_if_ahora_si_solo_en_dataset_fit_no_concateno_uf_sin_weights_quito_los_vistos_avisos_con_fecha_con_rank

--- challenge_postulacion_zonajobs/rank.py
+++ challenge_postulacion_zonajobs/rank.py
@@ -249,13 +249,13 @@ def predict_hard_users(
     inv_item_id_map = {v: k for k, v in item_id_map.items()}
 
     # for component in [10, 35, 50, 80, 100, 200]:
-    component = 40
+    component = 35
     model = lfm.LightFM(no_components=component, loss="warp", random_state=42)
     model.fit(
         interactions,
         # user_features=user_features,
         # sample_weight=weights,
-        epochs=100,
+        epochs=150,
         num_threads=8,
         verbose=True,
     )
